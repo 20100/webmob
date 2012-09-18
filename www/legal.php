46d81dbe86288192d512e7f1570e0bbe69c8995b
Vincent <cs.vg@free.fr>
2012-09-18 12:53:15 +0200

    First Test

---
+++ www/legal.php
@@ -0,0 +1,78 @@
+<!doctype html>
+<html lang="fr">
+<head>
+<title>Mon titre</title>
+<meta charset="utf-8" />
+<meta name="HandheldFriendly" content="True" />
+<meta name="MobileOptimized" content="320" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="jss/style.css" />
+<link rel="shortcut icon" href="img/favicon.ico" />
+<link rel="apple-touch-icon" href="img/logo-57.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="img/logo-72.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="img/logo-114.png" />
+<link rel="apple-touch-startup-image" href="img/splash-touch.png" />
+</head>
+<body>
+    <div class="wrapper"></div>
+    <br class="clear"/>
+    
+    <a id="header" href="index.php"/>
+      <img src="img/logo-72.png" class="logo" alt="logo myBd" title="Moteur de recherche de Bd"/>
+      <div class="titre"> <h1>MyBd.fr</h1> <h2> plein d'infos sur les BDs </h2>   </div>
+      <br class="clear"/>
+
+        <div class="wrapper">
+            <div id="menu">
+                <ul>
+                    <li><a href="search.php"> Search </a> </li>
+                    <li><a href="last.php">   Last   </a> </li>
+                    <li><a href="random.php">  Random </a> </li>
+                    <li><a href="legal.php">    Legal </a> </li>
+                </ul>
+            </div>
+        </div>
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+        
+            
+
+Lorem ipsum dolor sit amet, consectetur adipiscing elit. Duis eget libero justo. Aenean et sagittis ipsum. Suspendisse quam sapien, tincidunt eget auctor eu, facilisis at mi. Nam tempor nulla ac eros accumsan hendrerit. Nullam pharetra lorem a nisi porta eget volutpat diam accumsan. Praesent eget mollis elit. Donec sit amet pulvinar nisi. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos himenaeos. Nullam tempus ligula at nisi semper et eleifend lorem blandit. Ut tempus ullamcorper tincidunt. Praesent suscipit sem et nibh egestas ornare. Nullam in justo vel mauris pellentesque convallis. Donec vitae nulla mauris. Proin a metus ut lectus sodales tristique. Nullam sed elit lectus. In hac habitasse platea dictumst.
+
+Nunc bibendum fermentum arcu nec mollis. Sed sed est eu magna commodo malesuada. Duis luctus, metus a sodales mollis, libero velit elementum velit, at rhoncus arcu nibh id orci. Vivamus scelerisque auctor velit, non facilisis mauris condimentum a. Nulla volutpat quam non nunc tincidunt auctor. Maecenas a justo ac eros aliquet egestas vitae vitae dui. Cum sociis natoque penatibus et magnis dis parturient montes, nascetur ridiculus mus. Praesent ultricies eros ut velit auctor ut varius neque sagittis. Nam tristique varius sapien, et tincidunt leo faucibus sed. Nulla tellus magna, elementum et sagittis vitae, feugiat sed arcu.
+
+Morbi massa dui, accumsan non rutrum a, suscipit sed nulla. Suspendisse pulvinar dignissim dignissim. Ut eu nibh dolor. Curabitur nunc erat, placerat ac fringilla vel, gravida et lectus. Proin viverra sem eu libero molestie sed molestie tortor congue. Aliquam pharetra ultrices nisi non laoreet. Aliquam eleifend dui at augue ultrices quis fermentum turpis tincidunt. Nulla at massa sed dui feugiat aliquam vel eu enim.
+
+Nullam iaculis tellus vitae massa auctor imperdiet in eu massa. Suspendisse tincidunt cursus lorem sed mollis. Nunc suscipit sodales est in suscipit. Morbi luctus lacus lorem. Phasellus ut condimentum ipsum. Nullam imperdiet laoreet elit, a suscipit felis semper a. Suspendisse potenti. Mauris vel augue eu augue iaculis sodales. Sed non sapien diam, non fringilla libero. Donec dictum fringilla purus ac iaculis.
+
+Phasellus convallis, dui et pharetra hendrerit, sem quam tristique lorem, id tincidunt elit nisl quis risus. Ut lobortis tincidunt dolor ut vulputate. Donec accumsan diam id massa convallis luctus sed at velit. Nulla lorem nisi, scelerisque sit amet interdum id, scelerisque eget sem. Nulla bibendum, tortor nec ultrices volutpat, arcu tortor tincidunt metus, vel auctor dolor justo sed orci. Phasellus ante enim, adipiscing non tempor sit amet, cursus vel risus. Phasellus in ipsum eu libero feugiat ultricies quis non nibh.
+5 paragraphes, 414 mots, 2740 caractères de Lorem Ipsum généré<div id="footer">
+            <ul>
+                 <li><a href="search.php"> Search </a>    </li>
+                    <li><a href="last.php">   Last   </a> </li>
+                    <li><a href="random.php">  Random </a> </li>
+                    <li><a href="legal.php">    Legal </a> </li>
+                </ul>
+            </div>
+          
+</body>
+
+</html>
---
+++ www/search.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html lang="fr">
+<head>
+<title>Mon titre</title>
+<meta charset="utf-8" />
+<meta name="HandheldFriendly" content="True" />
+<meta name="MobileOptimized" content="320" />
+<meta name="viewport" content="width=device-width, initial-scale=1.0" />
+<link rel="stylesheet" href="jss/style.css" />
+<link rel="shortcut icon" href="img/favicon.ico" />
+<link rel="apple-touch-icon" href="img/logo-57.png" />
+<link rel="apple-touch-icon" sizes="72x72" href="img/logo-72.png" />
+<link rel="apple-touch-icon" sizes="114x114" href="img/logo-114.png" />
+<link rel="apple-touch-startup-image" href="img/splash-touch.png" />
+</head>
+<body>
+    <div class="wrapper"></div>
+    <br class="clear"/>
+    
+    <a id="header" href="index.php"/>
+      <img src="img/logo-72.png" class="logo" alt="logo myBd" title="Moteur de recherche de Bd"/>
+      <div class="titre"> <h1>MyBd.fr</h1> <h2> plein d'infos sur les BDs </h2>   </div>
+      <br class="clear"/>
+
+      
+      <form action="search.php" >
+          
+      </form>
+        <div class="wrapper">
+            <div id="menu">
+                <ul>
+                    <li><a href="search.php"> Search </a> </li>
+                    <li><a href="last.php">   Last   </a> </li>
+                    <li><a href="random.php">  Random </a> </li>
+                    <li><a href="legal.php">    Legal </a> </li>
+                </ul>
+            </div>
+            
+            
+            
+            
+            
+        </div>
+            <div id="footer">
+            <ul>
+                 <li><a href="search.php"> Search </a>    </li>
+                    <li><a href="last.php">   Last   </a> </li>
+                    <li><a href="random.php">  Random </a> </li>
+                    <li><a href="legal.php">    Legal </a> </li>
+                </ul>
+            </div>
+          
+</body>
+
+</html>
