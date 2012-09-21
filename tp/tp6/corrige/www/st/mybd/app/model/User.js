89acc3411ded557c8a5c6d74755843e1c99e3d9a
Vincent <cs.vg@free.fr>
2012-09-21 16:12:15 +0200

    1

---
+++ tp/tp6/corrige/www/st/mybd/app/model/User.js
@@ -0,0 +1,11 @@
+Ext.define('MyApp.model.User', {
+    extend: 'Ext.data.Model',
+    
+    config: {
+        fields: [
+            {name: 'id', type: 'int'},
+            {name: 'name', type: 'auto'},
+            {name: 'email', type: 'auto'}
+        ]
+    }
+});
